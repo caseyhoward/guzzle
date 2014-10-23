84939782be55ed52677cec3a33133fc7ce293a55
Casey Howard <caseyhoward@caseyhoward.com>
2014-10-22 22:34:31 -0700

    Refactor ProxyFactory

--- src/proxy_factory.js
+++ src/proxy_factory.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var _ = require('lodash');
 
 var camelCase = function(dashedString) {
   return dashedString.replace(/-([a-z])/g, function (match) {
@@ -19,6 +20,28 @@ module.exports = function() {
   };
 
   this.build = function(sources) {
+    function buildPluginProxy(pluginName) {
+      return function() {
+        stream = stream.pipe(gulpPlugins[pluginName].apply(null, arguments));
+        return proxy;
+      };
+    }
+
+    function buildProxyFunction(pluginName, property) {
+      return function() {
+        stream = stream.pipe(gulpPlugins[pluginName][property].apply(null, arguments));
+        return proxy;
+      }
+    }
+
+    function buildProxy(pluginName) {
+      var proxy = buildPluginProxy(pluginName);
+      _.each(_.functions(gulpPlugins[pluginName]), function(property) {
+        proxy[property] = buildProxyFunction(pluginName, property);
+      });
+      return proxy;
+    }
+
     var stream = gulp.src(sources);
     var proxy = {
       dest: function(destination) {
@@ -27,23 +50,7 @@ module.exports = function() {
       on: stream.on
     };
     for (pluginName in gulpPlugins) {
-      proxy[pluginName] = (function(pluginName) {
-        return function() {
-          stream = stream.pipe(gulpPlugins[pluginName].apply(null, arguments));
-          return proxy;
-        }
-      })(pluginName);
-
-      for (property in gulpPlugins[pluginName]) {
-        if (typeof gulpPlugins[pluginName][property] === 'function') {
-          proxy[pluginName][property] = (function(pluginName, property) {
-            return function() {
-              stream = stream.pipe(gulpPlugins[pluginName][property].apply(null, arguments));
-              return proxy;
-            }
-          })(pluginName, property);
-        }
-      }
+      proxy[pluginName] = buildProxy(pluginName);
     }
     return proxy;
   };
