b84074c9ce26cb050a9c8c6183c19d55231b399e
Casey Howard <caseyhoward@caseyhoward.com>
2014-10-22 22:34:12 -0700

    Update gulpfile to use task method from guzzle

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,5 @@
-var gulp = require('gulp');
 var guzzle = require('./index');
 
 guzzle.register('mocha');
 
-gulp.task('default', function () {
-  return guzzle.src('spec/acceptance/specs.js', {read: false}).mocha();
-});
+guzzle.task('default').src('spec/acceptance/specs.js', {read: false}).mocha();
