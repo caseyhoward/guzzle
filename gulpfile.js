c8f4ecc82136b08377d3aaf02b06c30e224a4175
Casey Howard <caseyhoward@caseyhoward.com>
2014-11-01 11:21:28 -0700

    Add gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var guzzle = require('./index');
+var gulp = require('gulp');
 
 guzzle.register('mocha');
 
 guzzle.task('default').src('spec/acceptance/specs.js', {read: false}).mocha();
+
+guzzle.task('watch', ['default'], function() {
+  gulp.watch(['spec/**/*.js'], ['default']);
+  gulp.watch(['src/**/*.js'], ['default']);
+});
