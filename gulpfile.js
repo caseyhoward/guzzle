6b28213e522169e058c74e5a20b8a54ca40fc8e9
Casey Howard <caseyhoward@caseyhoward.com>
2014-11-05 08:08:45 -0800

    Add gulp tasks for tagging

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var guzzle = require('./index');
 var gulp = require('gulp');
 
-guzzle.register('mocha');
+guzzle.register('mocha', 'tag-version', 'git', 'bump', 'filter');
 
 guzzle.task('default').src('spec/**/*.spec.js', {read: false}).mocha();
 
@@ -9,3 +9,16 @@ guzzle.task('watch', ['default'], function() {
   gulp.watch(['spec/**/*.js'], ['default']);
   gulp.watch(['src/**/*.js'], ['default']);
 });
+
+function incrementVersion(proxy, importance) {
+  return proxy.src(['./package.json'])
+    .bump({type: importance})
+    .dest('./')
+    .git.commit('Update version')
+    .filter('package.json')
+    .tagVersion();
+}
+
+incrementVersion(guzzle.task('patch'), 'patch');
+incrementVersion(guzzle.task('minor'), 'minor');
+incrementVersion(guzzle.task('major'), 'major');
